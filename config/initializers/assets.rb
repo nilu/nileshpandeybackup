55667fbeec67da6871acc10c98b569f9e0920c34
Nilesh Pandey <nlhpandey@gmail.com>
2015-02-04 14:42:19 -0500

    minify CSS

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = '1.1'
 Rails.application.config.assets.initialize_on_precompile = false
 
 # Precompile additional assets.
